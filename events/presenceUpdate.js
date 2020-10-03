1da548eb4d5ebab19e1cc8ad2514ffd71296f67d
Norbert Podsadowski <norbo11@googlemail.com>
2020-10-03 15:53:41 +0100

    Do not remove from queue when gather is in progress

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -2,13 +2,15 @@ const _ = require("lodash")
 const logger = require("../utils/logger")
 
 module.exports = (client, oldMember, newMember) => {
-    if (currentGather.currentQueue.includes(newMember.user)) {
+    if (!currentGather.gatherInProgress()) {
+        if (currentGather.currentQueue.includes(newMember.user)) {
 
-        if (newMember.presence.status !== "online") {
-            currentDiscordChannel.send(`<@${newMember.user.id}> changed status to \`${newMember.presence.status}\` and ` +
-                                       "was removed from the gather queue.")
+            if (newMember.presence.status !== "online") {
+                currentDiscordChannel.send(`<@${newMember.user.id}> changed status to \`${newMember.presence.status}\` and ` +
+                    "was removed from the gather queue.")
 
-            _.remove(currentGather.currentQueue, (x) => x === newMember.user)
+                _.remove(currentGather.currentQueue, (x) => x === newMember.user)
+            }
         }
     }
 }
