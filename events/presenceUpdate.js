fb2cb5e0283c1f1947df4cc4dd0600f81ab8edc5
Norbert Podsadowski <norbo11@googlemail.com>
2020-10-03 13:10:11 +0100

    Get presence working

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -1,14 +1,14 @@
 const _ = require("lodash")
 const logger = require("../utils/logger")
 
-module.exports = (client, oldPresence, newPresence) => {
+module.exports = (client, oldMember, newMember) => {
+    if (currentGather.currentQueue.includes(newMember.user)) {
 
-    if (currentGather.currentQueue.includes(newPresence.user)) {
-        if (newPresence.status !== "online") {
-            currentDiscordChannel.send(`<@${newPresence.user.id}> changed status to \`${newPresence.status}\` and 
-                                       was removed from the gather queue.`)
+        if (newMember.presence.status !== "online") {
+            currentDiscordChannel.send(`<@${newMember.user.id}> changed status to \`${newMember.presence.status}\` and ` +
+                                       "was removed from the gather queue.")
 
-            _.remove(currentGather.currentQueue, (x) => x === message.author)
+            _.remove(currentGather.currentQueue, (x) => x === newMember.user)
         }
     }
 }
