cd72496b635349be82f42a41d9eca295ef0aa36b
Norbert Podsadowski <norbo11@googlemail.com>
2020-10-23 17:29:45 +0100

    Deduplicate messages from server

--- utils/soldatEvents.js
+++ utils/soldatEvents.js
@@ -5,11 +5,16 @@ const soldat = require("./soldat2")
 PASSIVE_EVENTS = [
     {
         name: "player command",
-        pattern: /\[(?<playerName>.*?)] !(?<command>.*)/,
+        pattern: /\[(?<time>.*?)] \[(?<playerName>.*?)] !(?<command>.*)/,
         handler: (gather, match) => gather.playerCommand(match.groups["playerName"], match.groups["command"]),
-        condition: gather => true
+        condition: gather => true,
+        deduplicate: true
     },
 ]
+
+const DEDUPLICATE_INTERVAL_MS = 1000
+
+seenMessages = new Set()
 
 registerSoldatEventListeners = (gather, soldatClient) => {
     logger.log.info("Registered non-command event listeners.")
@@ -20,20 +25,31 @@ registerSoldatEventListeners = (gather, soldatClient) => {
         const message = new soldat.NetworkMessage(dataView.buffer);
         const type = message.ReadMessageType();
 
-        // TODO: We need to deduplicate messages here recieved in a short space of time because the server
-        //  seems to log things twice when we say something in-game
         if (type === soldat.MessageType.LogLine) {
             const text = soldat.NetworkMessage.ProcessLogLine(message);
 
             PASSIVE_EVENTS.forEach(eventSpec => {
                 let match = text.match(eventSpec.pattern)
                 if (match !== null && eventSpec.condition(gather)) {
+                    if (eventSpec.deduplicate) {
+                        if (seenMessages.has(text)) {
+                            logger.log.info(`Received duplicated ${eventSpec.name} event, ignoring: ${text}`)
+                            return
+                        }
+
+                        seenMessages.add(text)
+                        setTimeout(() => {
+                            seenMessages.delete(text)
+                        }, DEDUPLICATE_INTERVAL_MS)
+                    }
+
+                    logger.log.info(`Received ${eventSpec.name} event from server: ${text}`)
+
                     try {
                         eventSpec.handler(gather, match)
                     } catch (e) {
                         logger.log.error(`There was an error processing a ${eventSpec.name} event from the server: ${e.stack}`)
                     }
-                    logger.log.info(`Received ${eventSpec.name} event from server: ${text}`)
                 }
             })
         }
