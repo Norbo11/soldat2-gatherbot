6d525ea805a67049dcd8d3ae5baf565e53be2caf
Norbert Podsadowski <norbo11@googlemail.com>
2020-11-19 01:19:21 +0000

    On launching bot, modify server config with new webrcon credentials and start the server as a child process

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const fs = require("fs")
 const message = require("./events/message")
 const ready = require("./events/ready")
 const presenceUpdate = require("./events/presenceUpdate")
+const webrcon = require("./utils/webrcon")
+const child_process = require('child_process')
+const ini = require('ini')
 
 const client = new Discord.Client()
 
 
-setUpCommands = () => {
+const setUpCommands = () => {
     client.commands = []
 
     const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
@@ -20,18 +23,58 @@ setUpCommands = () => {
     }
 }
 
-cleanUp =  () => {
+const cleanUp = () => {
 }
 
-setUpEvents = () => {
+const setUpEvents = (webrconCredentials) => {
     client.on("message", (...args) => message(client, ...args))
-    client.once("ready", (...args) => ready(client, ...args))
+    client.once("ready", (...args) => ready(client, webrconCredentials, ...args))
     client.on("presenceUpdate", (...args) => presenceUpdate(client, ...args))
     process.on("SIGINT", cleanUp)
 }
 
-setUpCommands();
-setUpEvents();
-client.login(process.env.BOT_TOKEN)
+const setUpServer = (webrconCredentials) => {
+    const configFilename = `${process.env.SERVER_FOLDER}/autoconfig.ini`
+
+    // Trim the beginning of the file because there's some weird character there that messes up the parsing
+    const configContents = fs.readFileSync(configFilename, "utf-8").trimStart()
+    const config = ini.parse(configContents)
+    config.WebRcon.cKey = webrconCredentials.cKey
+
+    logger.log.info(`Modifying ${configFilename} with cKey ${webrconCredentials.cKey}`)
+    fs.writeFileSync(configFilename, ini.stringify(config, { whitespace: true }))
+
+    logger.log.info("Launching Soldat 2 process...")
+
+    // https://nodejs.org/api/child_process.html
+    const child = child_process.spawn(`${process.env.SERVER_FOLDER}/soldat2`, [], {
+
+        // Can use "true" if we want the S2 server to continue running after we shut down the but
+        detached: false,
+
+        // stdin, stdout, stderr are all ignored; server logs should be tailed via Logs/console.txt
+        stdio: ['ignore', 'ignore', 'ignore']
+    });
+
+    logger.log.info(`Spawned process with PID ${child.pid}`)
+}
+
+(async () => {
+    const webrconCredentials = await webrcon.fetchNewWebrconCredentials();
+
+    const afterServerSetup = async () => {
+        setUpCommands();
+        setUpEvents(webrconCredentials);
+
+        await client.login(process.env.BOT_TOKEN)
+    }
+
+    setUpServer(webrconCredentials);
+
+    // After 10 seconds, resume bot initialization. This is to prevent us from connecting to WebRcon too soon (before
+    // the server is up), etc.
+    setTimeout(afterServerSetup, 10_000)
+})()
+
 
 
--- utils/webrcon.js
+++ utils/webrcon.js
@@ -1,6 +1,6 @@
 const parser = require("node-html-parser")
 const Browser = require("zombie")
-
+const logger = require("./logger")
 
 const fetchNewWebrconCredentials = async () => {
     // We use a real browser here rather than pure GET requests, because we need to save/upload cookies and god knows
@@ -9,6 +9,7 @@ const fetchNewWebrconCredentials = async () => {
     // Without this the requests time out
     Browser.waitDuration = '30s'
 
+    logger.log.info("Fetching new WebRcon credentials...")
     const browser = new Browser()
     await browser.visit("https://www.webrcon.com/guest.php")
 
@@ -16,6 +17,7 @@ const fetchNewWebrconCredentials = async () => {
     const sessionId = root.querySelector('#sessionID').getAttribute("value")
     const cKey = root.querySelector('#ckey').getAttribute("value")
 
+    logger.log.info(`Got cKey ${cKey} and session ID ${sessionId}`)
     return {sessionId, cKey}
 }
 
