84425d63b269f9c08760051d7c5c01a5284f9787
Norbert Podsadowski <norbo11@googlemail.com>
2020-12-12 21:32:41 +0000

    Add a queue manager for handling multiple queues
    Signed-off-by: Norbert Podsadowski <norbo11@googlemail.com>

---
+++ game/queueManager.js
@@ -0,0 +1,121 @@
+import _ from "lodash";
+import discord from "../utils/discord";
+import logger from "../utils/logger"
+
+export class QueueManager {
+
+    constructor(discordChannel) {
+        this.discordChannel = discordChannel
+        this.servers = {}
+    }
+
+    addGatherServer(serverCode, gather) {
+        this.servers[serverCode] = {
+            code: serverCode,
+            queue: [],
+            gather,
+            size: 6
+        }
+    }
+
+    getServerWithLargestQueue() {
+        const unfilledQueues = _.filter(this.servers, server => !this.isQueueFilled(server))
+
+        if (unfilledQueues.length === 0) {
+            return null
+        }
+
+        const sorted = _.sortBy(_.values(unfilledQueues), server => -server.queue.length)
+        return sorted[0]
+    }
+
+    addToLargestQueue(discordUser) {
+        const server = this.getServerWithLargestQueue()
+        if (server === null) {
+            return null
+        }
+
+        this.addToQueue(discordUser, server.code)
+    }
+
+    displayQueue(server, rematch = false) {
+        const queue = server.queue
+
+        const queueMembers = queue.map(user => `<@${user.id}>`)
+        for (let i = 0; i < server.size - queue.length; i++) {
+            queueMembers.push(":bust_in_silhouette:")
+        }
+
+        this.discordChannel.send({
+            embed: {
+                title: "Gather Info",
+                color: 0xff0000,
+                fields: [
+                    {
+                        name: `**[${server.code}]** Current Queue ${rematch ? " (rematch)" : ""}`,
+                        value: `${queueMembers.join(" - ")}`
+                    },
+                    discord.getGameModeField(server.gather.gameMode),
+                ]
+            }
+        })
+    }
+
+    getQueue(code) {
+        const server = this.getServer(code)
+        return server.queue
+    }
+
+    getServer(code) {
+        return this.servers[code]
+    }
+
+    isQueueFilled(server) {
+        return server.queue.length >= server.size
+    }
+
+    addToQueue(discordUser, serverCode) {
+        this.remove(discordUser)
+
+        const server = this.getServer(serverCode)
+        const queue = server.queue
+
+        if (this.isQueueFilled(server)) {
+            return null
+        }
+
+        if (!queue.includes(discordUser)) {
+            queue.push(discordUser)
+
+            if (queue.length === server.size) {
+                server.gather.startNewGame().catch(e => logger.log.exception(e))
+            } else {
+                this.displayQueue(server)
+            }
+        }
+    }
+
+    findServerWithPlayer(discordUser) {
+        for (let server of _.values(this.servers)) {
+            for (let queue of server.queue) {
+                if (_.includes(server.queue, discordUser)) {
+                    return server
+                }
+            }
+        }
+
+        return null
+    }
+
+    remove(discordUser) {
+        const server = this.findServerWithPlayer(discordUser)
+
+        // Do nothing if player is not added
+        if (server !== null) {
+            _.remove(server.queue, (x) => x === discordUser)
+            this.displayQueue(server)
+        }
+    }
+
+
+}
---
+++ test/queueManager.js
@@ -0,0 +1,168 @@
+import chai from "chai";
+import {getTestDiscordChannel, getTestStatsDb, MockDiscordUser} from "../utils/testUtils";
+import sinon from 'sinon';
+import {QueueManager} from "../game/queueManager";
+import logger from "../utils/logger"
+import _ from "lodash"
+
+const assert = chai.assert
+
+
+describe("QueueManager", () => {
+    let statsDb = undefined
+    let currentTime = undefined
+    let mockCurrentTimestamp = undefined
+    let discordChannel = undefined
+    let players = undefined
+    let gather1 = undefined
+    let gather2 = undefined
+
+    beforeEach(async () => {
+        statsDb = await getTestStatsDb()
+        discordChannel = getTestDiscordChannel()
+
+        mockCurrentTimestamp = () => {
+            return currentTime;
+        }
+
+        players = []
+
+        for (let i = 0; i < 20; i++) {
+            players.push(new MockDiscordUser(`${i}`))
+        }
+
+        gather1 = sinon.stub()
+        gather1.startNewGame = () => {
+            return new Promise((resolve, reject) => {
+                logger.log.info("Started new gather")
+                resolve()
+            })
+        }
+
+        gather2 = sinon.stub()
+        gather2.startNewGame = () => {
+            return new Promise((resolve, reject) => {
+                logger.log.info("Started new gather")
+                resolve()
+            })
+        }
+    })
+
+    afterEach(() => {
+        statsDb.dropDatabase()
+    })
+
+    it("should add and remove players", async () => {
+        const queueManager = new QueueManager(discordChannel)
+        queueManager.addGatherServer("eu-1", sinon.stub())
+        queueManager.addGatherServer("eu-2", sinon.stub())
+
+        const queue1 = queueManager.getQueue("eu-1")
+        const queue2 = queueManager.getQueue("eu-2")
+        const player = players[0]
+
+        assert.deepEqual(queue1, [])
+        assert.deepEqual(queue2, [])
+
+        queueManager.addToLargestQueue(player)
+        assert.deepEqual(queue1, [player])
+
+        queueManager.addToLargestQueue(player)
+        assert.deepEqual(queue1, [player])
+
+        queueManager.remove(player)
+        assert.deepEqual(queue1, [])
+
+        queueManager.remove(player)
+        assert.deepEqual(queue1, [])
+
+    })
+
+    it("should ensure players are only ever present in one queue", async () => {
+        const queueManager = new QueueManager(discordChannel)
+        queueManager.addGatherServer("eu-1", sinon.stub())
+        queueManager.addGatherServer("eu-2", sinon.stub())
+
+        const queue1 = queueManager.getQueue("eu-1")
+        const queue2 = queueManager.getQueue("eu-2")
+        const player = players[0]
+
+        queueManager.addToQueue(player, "eu-1")
+        assert.deepEqual(queue1, [player])
+        assert.deepEqual(queue2, [])
+
+        queueManager.addToQueue(player, "eu-2")
+        assert.deepEqual(queue1, [])
+        assert.deepEqual(queue2, [player])
+    })
+
+    it("should fill up unfilled queues", async () => {
+        const queueManager = new QueueManager(discordChannel)
+
+        queueManager.addGatherServer("eu-1", gather1)
+        queueManager.addGatherServer("eu-2", gather2)
+
+        const queue1 = queueManager.getQueue("eu-1")
+        const queue2 = queueManager.getQueue("eu-2")
+
+        queueManager.addToQueue(players[0], "eu-1")
+        queueManager.addToQueue(players[1], "eu-1")
+        queueManager.addToQueue(players[2], "eu-1")
+        queueManager.addToQueue(players[3], "eu-1")
+        queueManager.addToQueue(players[4], "eu-1")
+        queueManager.addToQueue(players[5], "eu-1")
+
+        assert.deepEqual(queue1, _.filter(players, (player, i) => i <= 5))
+        assert.deepEqual(queue2, [])
+
+        queueManager.addToLargestQueue(players[6])
+
+        assert.deepEqual(queue1, _.filter(players, (player, i) => i <= 5))
+        assert.deepEqual(queue2, [players[6]])
+    })
+
+    it("should fail to add when all queues are full", async () => {
+        const queueManager = new QueueManager(discordChannel)
+
+        queueManager.addGatherServer("eu-1", gather1)
+        queueManager.addGatherServer("eu-2", gather2)
+
+        const queue1 = queueManager.getQueue("eu-1")
+        const queue2 = queueManager.getQueue("eu-2")
+
+        const sixPlayers1 = _.filter(players, (player, i) => i <= 5)
+        const sixPlayers2 = _.filter(players, (player, i) => i >= 6 && i <= 11)
+
+        for (let player of sixPlayers1) {
+            queueManager.addToQueue(player, "eu-1")
+        }
+
+        for (let player of sixPlayers2) {
+            queueManager.addToQueue(player, "eu-2")
+        }
+
+        assert.deepEqual(queue1, sixPlayers1)
+        assert.deepEqual(queue2, sixPlayers2)
+
+        const result = queueManager.addToLargestQueue(players[6])
+        assert.isNull(result)
+    })
+
+    it("should fail to add to full queue", async () => {
+        const queueManager = new QueueManager(discordChannel)
+        queueManager.addGatherServer("eu-1", gather1)
+
+        const queue1 = queueManager.getQueue("eu-1")
+        const sixPlayers1 = _.filter(players, (player, i) => i <= 5)
+
+        for (let player of sixPlayers1) {
+            queueManager.addToQueue(player, "eu-1")
+        }
+
+        assert.deepEqual(queue1, sixPlayers1)
+
+        const result = queueManager.addToQueue(players[6], "eu-1")
+        assert.isNull(result)
+    })
+
+})
