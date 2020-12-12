27373fcef624a84cd025f435b7abd90b0240deea
Norbert Podsadowski <norbo11@googlemail.com>
2020-12-12 23:25:55 +0000

    Convert !del command to use new queue manager
    Signed-off-by: Norbert Podsadowski <norbo11@googlemail.com>

--- commands/remove.js
+++ commands/remove.js
@@ -6,12 +6,6 @@ export default {
     aliases: ["del", "remove"],
     description: "Remove yourself from the gather queue.",
     execute(client, message, args) {
-        if (currentGather.gatherInProgress()) {
-            message.channel.send("A gather is currently in progress.")
-            return
-        }
-
-        _.remove(currentGather.currentQueue, (x) => x === message.author)
-        utils.displayQueueWithServerInfo(message)
+        currentQueueManager.remove(message.author)
     },
 };
