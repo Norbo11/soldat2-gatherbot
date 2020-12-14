09c9bd83dc688efd757d51a37bf13ba02bbefe71
Norbert Podsadowski <norbo11@googlemail.com>
2020-12-14 21:38:07 +0000

    Broader try-catch when initializing servers

--- events/ready.js
+++ events/ready.js
@@ -28,16 +28,16 @@ export default (client) => {
         currentDiscordChannel.send("Initializing Soldat 2 Gather Bot...").catch(e => logger.log.error(`Could not send initialization message (${e.message}):\n${util.inspect(e.response)}`))
 
         for (let serverConfig of config.servers) {
-            const {sessionId, cKey, pid, port} = await resolveServerStrategy(serverConfig)
-            const server = currentQueueManager.createGatherServer(serverConfig, pid, port)
-            logger.log.info(`Connecting with server ${serverConfig.code} using sessionId ${sessionId} and cKey ${cKey}`)
-
-            // This await is crucial. It blocks until the connection with the current server is fully established and
-            // we've received a response to our initialization command/ping message. Without this, we will only be
-            // able to connect to one server, and the others will throw errors. Probably some poor implementation on
-            // WebRcon side. Initially I thought it was http connection pools and/or TLS session caching causing some
-            // problems, but I did a fair amount of digging to try and disable those, to no avail.
             try {
+                const {sessionId, cKey, pid, port} = await resolveServerStrategy(serverConfig)
+                const server = currentQueueManager.createGatherServer(serverConfig, pid, port)
+                logger.log.info(`Connecting with server ${serverConfig.code} using sessionId ${sessionId} and cKey ${cKey}`)
+
+                // This await is crucial. It blocks until the connection with the current server is fully established and
+                // we've received a response to our initialization command/ping message. Without this, we will only be
+                // able to connect to one server, and the others will throw errors. Probably some poor implementation on
+                // WebRcon side. Initially I thought it was http connection pools and/or TLS session caching causing some
+                // problems, but I did a fair amount of digging to try and disable those, to no avail.
                 const gather = await initializeServer(server, sessionId, cKey)
                 currentQueueManager.addGatherServer(server, gather)
             } catch (e) {
